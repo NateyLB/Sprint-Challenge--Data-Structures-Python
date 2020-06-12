1df0376e2a9df0ee1fbafe626562860a03066665
Nathan <nateyl.b@gmail.com>
2020-06-12 10:09:57 -0700

    finishd names

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,18 @@
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.data = []
+        for x in range(capacity):
+            self.data.append(x)
+        self.current=0
 
     def append(self, item):
-        pass
+        if self.current == self.capacity:
+            self.current = 0
+        self.data[self.current] = item
+        self.current += 1
+        print(self.current)
 
     def get(self):
-        pass
+        return self.data
