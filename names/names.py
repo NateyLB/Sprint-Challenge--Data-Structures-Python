1248a15d7d12bc0272b221af29900ff3e0d28a1b
Nathan <nateyl.b@gmail.com>
2020-06-12 10:07:38 -0700

    finished with names

--- names/names.py
+++ names/names.py
@@ -1,4 +1,8 @@
 import time
+import sys
+sys.path.append('.')
+from binary_search_tree import BSTNode
+
 
 start_time = time.time()
 
@@ -9,14 +13,25 @@ f.close()
 f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
+binary_name2 = BSTNode("dummy")
+
+for x in names_2:
+    binary_name2.insert(x)
+
 
 duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
+#Replace the nested for loops below with your improvements
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    if binary_name2.contains(name_1):
+        duplicates.append(name_1)
+    else:
+        pass
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -26,3 +41,4 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+#print(set(names_1) & set(names_2))
